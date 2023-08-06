43b98667967caa39f90469cb5a4cc24eabea5688
SondreB <sondre@outlook.com>
2023-08-06 20:40:42 +0200

    Add PWA and icons

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -1,13 +1,15 @@
 import { MongoClient } from "mongodb";
 
-const connectionString = process.env.ATLAS_URI || "";
+const connectionString = process.env.MARKET_DATABASE || "";
+
+console.log(connectionString);
 
 const client = new MongoClient(connectionString);
 
 let conn;
 try {
   conn = await client.connect();
-} catch(e) {
+} catch (e) {
   console.error(e);
 }
 
