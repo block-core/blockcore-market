f2c84e3c5f4b02632d96b1856e60a5794a7cb738
SondreB <sondre@outlook.com>
2023-08-06 21:56:30 +0200

    Prepare for Docker Hub publication

--- server/index.mjs
+++ server/index.mjs
@@ -3,6 +3,13 @@ import cors from "cors";
 import "./loadEnvironment.mjs";
 import "express-async-errors";
 import posts from "./routes/posts.mjs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+console.log(__filename);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -10,15 +17,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Load the /posts routes
 app.use("/posts", posts);
 
-// Global error handling
-app.use((err, _req, res, next) => {
-  res.status(500).send("Uh oh! An unexpected error occured.")
-})
+app.use("/", express.static(path.join(__dirname, "../app/dist/app")));
+// app.get("/*", (req, res) => res.sendFile(path.join(__dirname)));
 
-// start the Express server
+app.use((err, _req, res, next) => {
+  res.status(500).send("Uh oh! An unexpected error occured.");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
